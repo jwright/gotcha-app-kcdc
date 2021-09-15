de6f51324abd6f67e79c495fdd22f3827f27cbb5
Jamie Wright <jamie@brilliantfantastic.com>
2021-09-15 14:13:23 -0500

    Client helper library

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ApolloProvider } from '@apollo/client'
 
 import ArenaListScreen from "./screens/ArenaListScreen";
 import HomeScreen from "./screens/HomeScreen";
 
+import client from "./helpers/client"
+
 const Stack = createNativeStackNavigator();
 
 const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="ArenaList" component={ArenaListScreen} options={{ headerShown: false, title: "Select an Arena" }} />
-      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false, title: "Gotcha!" }} />
-    </Stack.Navigator>
-  </NavigationContainer>
+  <ApolloProvider client={client}>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="ArenaList" component={ArenaListScreen} options={{ headerShown: false, title: "Select an Arena" }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false, title: "Gotcha!" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </ApolloProvider>
 )
 
 export default App;
