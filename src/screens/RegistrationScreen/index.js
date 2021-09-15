bb74b30ff40d47bad9263fe12bb1a85a07d5a518
Jamie Wright <jamie@brilliantfantastic.com>
2021-09-15 15:49:29 -0500

    Display registration form and pass bacl registration data

--- src/screens/RegistrationScreen/index.js
+++ src/screens/RegistrationScreen/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 
+import RegistrationForm from "../../components/RegistrationForm";
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -8,11 +10,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    alignSelf: 'center',
+    fontSize: 40,
+  }
 });
 
 const RegistrationScreen = () => (
   <View style={styles.container}>
-    <Text>Register them playas</Text>
+    <Text style={styles.header}>Player Registration</Text>
+    <RegistrationForm onRegistration={(player) => console.log("Register", player)} />
   </View>
 )
 
